02fe8a07ce6de9944f87c0c8ec085b1bf8329a0f
octalsrc <nicklewchenko92@gmail.com>
2016-11-02 22:03:09 -0600

    all experiments

--- app/src/main/java/edu/upenn/aradha/starling/MainActivity.java
+++ app/src/main/java/edu/upenn/aradha/starling/MainActivity.java
@@ -3,42 +3,19 @@ package edu.upenn.aradha.starling;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Environment;
 
-import java.io.File;
-import java.io.FileWriter;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import edu.colorado.plv.droidStar.LearningPurpose;
 import edu.upenn.aradha.starling.droidStar.lp.SpeechRecognizerLP;
-import edu.upenn.aradha.starling.droidStar.lp.MediaPlayerLP;
-import edu.upenn.aradha.starling.droidStar.lp.MinusStopTestLP;
 import edu.upenn.aradha.starling.droidStar.lp.CountDownTimerLP;
-import edu.upenn.aradha.starling.droidStar.lp.AsyncTaskLP;
 import edu.upenn.aradha.starling.droidStar.lp.AsyncTaskNoGetLP;
-import edu.upenn.aradha.starling.droidStar.lp.AsyncTaskLongLP;
 import edu.upenn.aradha.starling.droidStar.lp.SpellCheckerSessionLP;
-import edu.upenn.aradha.starling.droidStar.lp.DoubleSpellCheckerSessionLP;
 import edu.upenn.aradha.starling.droidStar.lp.SQLiteOpenHelperLP;
 import edu.upenn.aradha.starling.droidStar.lp.FileObserverLP;
 
-import edu.upenn.aradha.starling.lstar.DistinguisherBoundedEquivalenceOracle;
-import edu.upenn.aradha.starling.lstar.EquivalenceOracle;
-import edu.upenn.aradha.starling.lstar.Input;
-import edu.upenn.aradha.starling.lstar.LStar;
-import edu.upenn.aradha.starling.lstar.MembershipOracle;
-import edu.upenn.aradha.starling.droidStar.TransducerAdapter;
-
-import edu.upenn.aradha.starling.droidStar.Static;
-
 public class MainActivity extends AppCompatActivity {
-
     public static boolean started = false;
-
-    public static String OUTDIR = Environment.DIRECTORY_DOCUMENTS;
 
     @Override
     public void onStart() {
@@ -49,8 +26,12 @@ public class MainActivity extends AppCompatActivity {
 
         if (weCanWrite()) {
             List<LearningPurpose> purposes = new ArrayList();
+            purposes.add(new SpeechRecognizerLP(this));
+            purposes.add(new CountDownTimerLP(this));
+            purposes.add(new AsyncTaskNoGetLP(this));
+            purposes.add(new SpellCheckerSessionLP(this));
+            purposes.add(new SQLiteOpenHelperLP(this));
             purposes.add(new FileObserverLP(this));
-            // purposes.add(new SpeechRecognizerLP(this));
 
             Experiment.experiment(this, purposes);
         } else {
