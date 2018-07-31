cc12c5ec5d0a8c61f3108c76ced3cf98a732e5d5
octalsrc <nicklewchenko92@gmail.com>
2018-07-31 17:21:35 -0600

    Add SQLiteOpenHelper activity for demo

---
+++ driver-app/src/main/scala/edu/colorado/plv/droidstar/experiments/SQLiteOpenHelperActivity.scala
@@ -0,0 +1,33 @@
+package edu.colorado.plv.droidstar
+package experiments
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+import scala.collection.JavaConverters._
+
+import android.os.Environment
+
+class SQLiteOpenHelperActivity extends AppCompatActivity {
+
+  // List learning purposes you wish to learn here.  They will be
+  // performed in the order they are listed.
+  lazy val experiments = Seq[LearningPurpose](
+    new lp.SQLiteOpenHelperLP(this)
+      // etc.
+  )
+
+
+  override def onCreate(s: Bundle): Unit = {
+    super.onCreate(s)
+    weCanWrite() match {
+      case true => experiment(this, experiments.asJava)
+      case false => println("Seems we can't report results...")
+    }
+  }
+
+  def weCanWrite(): Boolean =
+    Environment
+      .getExternalStorageState()
+      .equals(Environment.MEDIA_MOUNTED)
+}
